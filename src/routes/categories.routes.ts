3e8771c52514d1576fa245da433fc76505f40cf9
kleyson castro <kleysoncastro99@gmail.com>
2022-02-02 23:07:34 -0300

    aplly word soloid S and D 	separate responsabilite your category.routes for service 	config prettier semi true duble quoate

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,18 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../service/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
-const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
 
-    const categoryAlreadyExist = categoriesRepository.findByName(name);
-
-    if (categoryAlreadyExist) {
-        return res.status(400).json({ error: "Category already axists" });
-    }
-
-    categoriesRepository.create({ name, description });
+    const createCategoryService = new CreateCategoryService(
+        categoriesRepository
+    );
+    createCategoryService.execute({ name, description });
     return res.status(201).send();
 });
 
