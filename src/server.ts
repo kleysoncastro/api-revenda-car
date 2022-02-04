daafe556d5fdf9c7cc1545111513f770672c364a
kleyson castro <kleysoncastro99@gmail.com>
2022-02-04 15:18:18 -0300

    fac: move router for file index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRouter } from "./routes/specification.routes";
+import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
-
-app.use("/categories", categoriesRoutes);
-app.use("/especification", specificationRouter);
-
+app.use(router);
 app.listen(3333, () => console.log("running server"));
