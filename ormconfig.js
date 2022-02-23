fed8f38769b190bd58b479d5764f199b2a0eca7b
kleyson Castro <kleysoncastro99@gmail.com>
2022-02-23 16:03:00 -0300

    add migration for Categories

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,13 @@
 require("dotenv").config()
 
-console.log(process.env.POSTGRES_DB)
 module.exports = {
     "type": "postgres",
     "host": "localhost",
     "port": "5432",
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB
+    "database": process.env.POSTGRES_DB,
+    "cli": {
+        "migrationsDir": "./src/database/migrations"
+    }
  }
